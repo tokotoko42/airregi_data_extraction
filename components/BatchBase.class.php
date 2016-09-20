c78932379258f0bfc07b3636647d7f8b6f7aadd6
Kamijo Tetsuya <kamijo42@gmail.com>
2016-09-19 21:28:59 -0400

    Update base file

--- components/BatchBase.class.php
+++ components/BatchBase.class.php
@@ -2,17 +2,11 @@
 /**
  * Batch base class.
  *
- * @package
- * @subpackage
- * @version $Revision$
  */
 class BatchBase extends CConsoleCommand
 {
-    public  $run_multiple = true;
     public  $exit_code = 0;
     public  $log_id = '';
-
-    private $lock_file;
     public  $request_id = '';
 
     public function init()
@@ -23,37 +17,11 @@ class BatchBase extends CConsoleCommand
         register_shutdown_function(function() {
             Yii::app()->end();
         });
-        if (!$this->run_multiple) {
-            $this->checkIsRunning();
-        }
+
         // ログ出力用 リクエスト固有ID生成
         $this->request_id = md5(time(). rand(0, 10000));
 
         Yii::log('Batch ' . $this->name . ' start', 'info');
-    }
-
-    private function checkIsRunning()
-    {
-        if (!isset(Yii::app()->params['batch_lock_dir'])) {
-            echo "Lock Directory not found in configulation\n";
-            Yii::app()->end();
-        }
-        $this->lock_file = Yii::app()->params['batch_lock_dir'] . '/batch_' . $this->name . '.pid';
-        $pid = null;
-        if (file_exists($this->lock_file)) {
-            $pid = file_get_contents($this->lock_file);
-        }
-        if ($pid) {
-            if (file_exists('/proc/' . $pid)) {
-                Yii::log('batch '. $this->name. ' is still running.(PID:'. $pid. ')', 'error', 'components.batchbase');
-                $this->exit_code = 1;
-                Yii::app()->end();
-            } else {
-                Yii::log('previous batch '. $this->name. ' could stop by unknown reason', 'error', 'components.batchbase');
-                unlink($this->lock_file);
-            }
-        }
-        file_put_contents($this->lock_file, getmypid());
     }
 
     protected function setLog($log_id, $log_level, $cls, $fnc, $line, $message)
